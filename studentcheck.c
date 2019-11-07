f16124ef9397807cb5350ad3a421b25d8cab326d
gunhee72 <56633202+gunhee72@users.noreply.github.com>
2019-11-07 11:52:59 +0900

    Add files via upload

---
+++ studentcheck.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <windows.h>
+
+void textcolor(int color_number)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_number);
+} //텍스트 색 설정
+
+void main()
+{
+	system("mode con cols=50 lines=30");
+	textcolor(11);
+	printf("▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤\n");
+	printf("                                                  \n");
+	printf("                                                  \n");
+	printf("                  -  학생 조회  -                 \n");
+	printf("                                                  \n");
+	printf("                                                  \n");
+	printf("                학번을 입력해주세요               \n");
+	printf("                                                  \n");
+	printf("                                                  \n");
+	printf("                    2019111441                    \n");
+	printf("                    ==========                    \n");
+	printf("                                                  \n");
+	printf("--------------------------------------------------\n");
+	printf("                                                  \n");
+	printf("                                                  \n");
+	printf("                 이름 : 이건희                    \n");
+	printf("                                                  \n");
+	printf("                   키 : 1234                      \n");
+	printf("                                                  \n");
+	printf("                                                  \n");
+	printf("                                                  \n");
+	printf(" KNU                                              \n");
+	printf("▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤\n");
+}
+/*
+void main()
+{
+	system("mode con cols=50 lines=30");
+	textcolor(11);
+	printf("▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤\n");
+	printf("                                                  \n");
+	printf("                                                  \n");
+	printf("                  -  학생 조회  -                 \n");
+	printf("                                                  \n");
+	printf("                                                  \n");
+	printf("                학번을 입력해주세요               \n");
+	printf("                                                  \n");
+	printf("                                                  \n");
+	printf("                    2019111441                    \n");
+	printf("                    ==========                    \n");
+	printf("                                                  \n");
+	printf("--------------------------------------------------\n");
+	printf("                                                  \n");
+	printf("                                                  \n");
+	printf("             조회된 학생정보가 없습니다           \n");
+	printf("                                                  \n");
+	printf("                                                  \n");
+	printf(" KNU                                              \n");
+	printf("▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤\n");
+}*/
